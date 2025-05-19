beb82a9a9a78e551ad1e165604388fd97e80f232
gabrielssmacedo <gabrielmacedo317@gmail.com>
2025-05-19 17:29:58 -0300

    feat: add Position class

--- src/app/Program.java
+++ src/app/Program.java
@@ -1,10 +1,12 @@
 package app;
 
+import boardgame.Position;
+
 public class Program {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hello world");
+		System.out.println(new Position(3, 5));
 	}
 
 }
