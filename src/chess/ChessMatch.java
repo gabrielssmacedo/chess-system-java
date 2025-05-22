59fbdcedf2f6b15b1c881a77fe73b3a7a6248ee4
gabrielssmacedo <gabrielmacedo317@gmail.com>
2025-05-22 19:43:35 -0300

    feat: implements possible moves of Queen

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -11,6 +11,7 @@ import chess.pieces.Bishop;
 import chess.pieces.King;
 import chess.pieces.Knight;
 import chess.pieces.Pawn;
+import chess.pieces.Queen;
 import chess.pieces.Rook;
 
 public class ChessMatch {
@@ -197,6 +198,9 @@ public class ChessMatch {
 	
 		placeNewPiece('a', 8, new Rook(board, Color.BLACK));
         placeNewPiece('h', 8, new Rook(board, Color.BLACK));
+        
+        placeNewPiece('d', 8, new Queen(board, Color.BLACK));
+        placeNewPiece('d', 1, new Queen(board, Color.WHITE));
         
         placeNewPiece('b', 8, new Knight(board, Color.BLACK));
         placeNewPiece('g', 8, new Knight(board, Color.BLACK));
